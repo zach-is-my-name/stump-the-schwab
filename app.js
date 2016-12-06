c198ed1ac0649e67f245d06dae375e685b6a0d7f
sgtquezada <sgtquezada@gmail.com>
2016-12-05 22:24:35 -0800

    updated check answer function

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ $(document).ready(function() {
             {
                 questionTitle: 'Who won the World Series in 2004?',
                 number: 1,
-                answer: 0, // index of the multiple_choice array
-                multiple_choice: ['red sox', 'yankees', 'giants', 'dodgers']
+                answer: 2, // index of the multiple_choice array
+                multiple_choice: ['giants', 'yankees', 'red sox', 'dodgers']
             }, {
                 questionTitle: 'What women\'s tennis player holds the world record for most grandslam championship',
                 number: 2,
@@ -21,25 +21,42 @@ $(document).ready(function() {
                 number: 4,
                 answer: 2,
                 multiple_choice: ['25', '20', '33', '35']
-            }
+            },
+              {
+                  questionTitle: 'What was the highest-scoring game in NBA history?',
+                  number: 5,
+                  answer: 2,
+                  multiple_choice: [150, 213, 370, 186]
+              }
         ],
         user_answer: '',
         runningScore: 0,
-        correct_message: 'Correct!',
-        incorrect_message: 'Incorrect!'
+        correctMessage: 'Correct!',
+        incorrectMessage: 'Incorrect!'
     }
 
     function renderHTML(state, question) {
         var startOver = false;
-        var question = state.questions[0];
+        var question = state.questions[question];
         var multipleChoices = question.multiple_choice;
         var title = question.questionTitle;
-        var questionNumber = question.number;
+        var questionNumber = state.questions.indexOf(question);
         var answer = question.answer;
+        var score = state.runningScore;
+        var totalQuestions = state.questions.length;
         var inputs = multipleChoices.map(function(choice) {
             return '<input type="radio" class="user_input" name="question" value="' + choice + '">&nbsp' + choice + '<br />';
         });
-        var html = '<form> <h2>' + title + '</h2>'
+
+        var html = '<div class="score-info"> \
+                      <p> Score: ' + score + '/' + totalQuestions + '</p> \
+                    </div> \
+                    <div class="question-info">  \
+                      <p> Question ' + questionNumber + ' out of ' + totalQuestions + '</p> \
+                      <span style="display:none">' + questionNumber + '</span> \
+                    </div> <br /> <br />\
+                    <form> <h2>' + title + '</h2>'
+
         for (var i = 0; i < inputs.length; i++) {
           html += inputs[i];
         }
@@ -49,28 +66,29 @@ $(document).ready(function() {
 
     function checkAnswer(state, questionNumber, userAnswer) {
         var question = state.questions[questionNumber];
-        var answer = question.answer;
+        var answer = question.multiple_choice[question.answer];
         var message = '';
         if (answer === userAnswer) {
             // update the score
             question.runningScore++;
-            message = question.correct_message;
+            message = state.correctMessage;
         } else {
-            message = questions.incorrect_message;
+            message = state.incorrectMessage;
         }
         return message;
     }
 
     $('div').on('click', '.start', function(event) {
-        renderHTML(state, 1);
+        renderHTML(state, 0);
     });
 
     $('.js-question-box').on('click', '.js-submit-answer', function(event) {
         event.preventDefault();
         event.stopPropagation();
         var questionNumber = $(this).closest('div.js-question-box').find('span').text();
-        console.log(questionNumber);
-        var userAnswer = $(this).closest('div.js-question-box').find('input[name="question-1"]:checked').val();
+        var userAnswer = $(this).closest('div.js-question-box').find('input[name="question"]:checked').val();
+        var response = checkAnswer(state, questionNumber, userAnswer);
+        console.log(response);
     })
 
 
